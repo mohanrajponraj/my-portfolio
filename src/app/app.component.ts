6468cb106f7a24b01657251c9528bc7e4570ebeb
mohanrajponraj <mohanrithik18@gmail.com>
2025-07-18 16:07:56 +0530

    Initial commit for portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBarComponent, ResumeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-portfolio';
+
+  constructor(
+    private router: Router
+  ){}
+
+  // OnSidebar(){
+  //   this.router.navigate(['side-bar'])
+  // }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'side-bar', component: SideBarComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'resume', component: ResumeComponent},
+    {path: 'home', loadChildren:()=> import('./home/home.module').then(m=> m.HomeModule)}
+];
