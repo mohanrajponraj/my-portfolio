9847078c74c9dcf105e011196d6c33f33ebf9ccd
mohanrajponraj <mohanrithik18@gmail.com>
2025-07-22 19:04:52 +0530

    sidebar UI design changes.

---
+++ src/app/side-bar/side-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-side-bar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.css'
+})
+export class SideBarComponent {
+
+  constructor(private router: Router){}
+
+}
